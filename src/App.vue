c725e34fa8b5fabc8b9c605a69429d9ef1962984
adecode <agorohabeeb@gmail.com>
2022-02-02 11:50:35 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container">
+      <AddTodo/>
+      <FilterTodos/>
+      <Todo/>
     </div>
-    <router-view/>
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/todo">Todo</router-link>
+    </div> -->
+    <!-- <router-view/> -->
   </div>
 </template>
 
+<script>
+import Todo from './components/Todo.vue'
+import AddTodo from './components/AddTodo.vue'
+import FilterTodos from './components/FilterTodos.vue'
+
+export default {
+  name: "app",
+  components:{
+    Todo,
+    AddTodo,
+    FilterTodos
+  }
+  
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body{
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
 }
 
-#nav {
-  padding: 30px;
+.container{
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/Todo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <Todo/>
+    </div>
+</template>
+
+<script>
+import Todo from '../components/Todo.vue'
+export default {
+    components:{
+        Todo,
+    }
+}
+</script>
